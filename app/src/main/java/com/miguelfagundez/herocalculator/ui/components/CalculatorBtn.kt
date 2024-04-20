5b8e312287af3950dff03d6adf00a44a2055ea50
Miguel Fagundez <59711011+miguelfagundez@users.noreply.github.com>
2024-04-20 13:37:02 -0400

    Create CalculatorBtn.kt

---
+++ app/src/main/java/com/miguelfagundez/herocalculator/ui/components/CalculatorBtn.kt
@@ -0,0 +1,43 @@
+package com.miguelfagundez.herocalculator.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CalculatorButton(
+    // Calculator symbol suc as number, text, etc
+    symbol: String,
+    // Button info such as background color, shape, spacing, etc
+    info: Modifier,
+    // Button Size by default 36 units
+    btnSize: TextUnit = 36.sp,
+    // Button symbol color by default is white
+    symbolColor: Color = Color.White,
+    // Button action/event
+    onClick: () -> Unit
+){
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(CircleShape)
+            .clickable { onClick }
+            // Apply our modifier at the end of component properties.
+            // First circle shape and onclick event is applied
+            .then(info)
+    ){
+        Text(
+            text = symbol,
+            fontSize = btnSize,
+            color = symbolColor
+        )
+    }
+}
